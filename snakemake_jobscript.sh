5ed99bd452ba2c08039d889bd285811397139ec0
Logan Brase <logan.brase@manchester.ac.uk>
2025-05-27 10:48:23 +0100

    add __init.py__ file from 3UTRBERT which has the metrics functions

--- snakemake_jobscript.sh
+++ snakemake_jobscript.sh
@@ -8,14 +8,14 @@
 #####################################################
 ### run command
 #####################################################
-#singularity exec -B $(readlink motif_seqs.fasta):/tmp/motif_seqs.fasta -B $(readlink control_seqs.fasta):/tmp/control_seqs.fasta docker://memesuite/memesuite ame --oc ame_output --control /tmp/control_seqs.fasta --evalue-report-threshold 100 --method fisher --rna /tmp/motif_seqs.fasta /opt/meme/share/meme-5.5.7/db/motif_databases/RNA/Ray2013_rbp_Drosophila_melanogaster.meme
-#  --hit-lo-fraction 0.1 --scoring max 
+source ~/.bashrc
 conda activate inseq
 #~/.local/share/mamba/bin/dvc repro runAME_highLowDecay #dvc exp run
 #~/.local/share/mamba/bin/dvc repro randomizeSeqsAndExtraFeatures #dvc exp run
-~/.local/share/mamba/bin/dvc repro fineTuneModel
-~/.local/share/mamba/bin/git add -u
-~/.local/share/mamba/bin/git add dvclive/
-~/.local/share/mamba/bin/git add data/
-~/.local/share/mamba/bin/git add output/data/
+cd /mnt/mr01-home01/m65338lb/projects/mRNA_LLM-worktrees/translationEfficiency/
+~/.local/share/mamba/bin/dvc exp run --run-all --jobs 1
+#~/.local/share/mamba/bin/git add -u
+#~/.local/share/mamba/bin/git add dvclive/
+#~/.local/share/mamba/bin/git add data/
+#~/.local/share/mamba/bin/git add output/data/
 #~/.local/share/mamba/bin/git commit -m "1 count in each replicate (mRNA and ribo), double log transform"
