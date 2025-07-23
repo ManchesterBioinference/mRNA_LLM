f7143d7d95064c388531105a17899af980581a80
Logan Brase <logan.brase@manchester.ac.uk>
2025-07-23 09:37:08 +0100

    (ablations) default - all extra features included

--- snakemake_jobscript.sh
+++ snakemake_jobscript.sh
@@ -1,35 +1,35 @@
 #!/bin/bash --login
 
 #SBATCH -p gpuL #multicore #
-#SBATCH -G 1
+#SBATCH -G 2
 #SBATCH -n 4
-#SBATCH -t 1:00:00
+#SBATCH -t 6:00:00
 
 #####################################################
 ### run command
 #####################################################
 source ~/.bashrc
 conda activate inseq
-~/.local/share/mamba/bin/dvc repro predict@0 # visualizeImportance #runViennaRNA #
+~/.local/share/mamba/bin/dvc repro fineTuneModel # visualizeImportance #runViennaRNA #
 ~/.local/share/mamba/bin/git add -u
-~/.local/share/mamba/bin/git commit -m "no_mfe"
+~/.local/share/mamba/bin/git commit -m "(ablations) default - all extra features included"
 
-~/.local/share/mamba/bin/dvc repro predict@1 # visualizeImportance #runViennaRNA #
+~/.local/share/mamba/bin/dvc repro -s -f fineTuneModel_ablations@0 # visualizeImportance #runViennaRNA #
 ~/.local/share/mamba/bin/git add -u
-~/.local/share/mamba/bin/git commit -m "no_lengths"
+~/.local/share/mamba/bin/git commit -m "(ablations) no_mfe"
 
-~/.local/share/mamba/bin/dvc repro predict@2 # visualizeImportance #runViennaRNA #
+~/.local/share/mamba/bin/dvc repro -s -f fineTuneModel_ablations@1 # visualizeImportance #runViennaRNA #
 ~/.local/share/mamba/bin/git add -u
-~/.local/share/mamba/bin/git commit -m "no_gc_content"
+~/.local/share/mamba/bin/git commit -m "(ablations) no_lengths"
 
-~/.local/share/mamba/bin/dvc repro predict@3 # visualizeImportance #runViennaRNA #
+~/.local/share/mamba/bin/dvc repro -s -f fineTuneModel_ablations@2 # visualizeImportance #runViennaRNA #
 ~/.local/share/mamba/bin/git add -u
-~/.local/share/mamba/bin/git commit -m "no_codons"
+~/.local/share/mamba/bin/git commit -m "(ablations) no_gc_content"
 
-~/.local/share/mamba/bin/dvc repro predict@4 # visualizeImportance #runViennaRNA #
+~/.local/share/mamba/bin/dvc repro -s -f fineTuneModel_ablations@3 # visualizeImportance #runViennaRNA #
 ~/.local/share/mamba/bin/git add -u
-~/.local/share/mamba/bin/git commit -m "seq_only"
+~/.local/share/mamba/bin/git commit -m "(ablations) no_codons"
 
-~/.local/share/mamba/bin/dvc repro predict@5 # visualizeImportance #runViennaRNA #
+~/.local/share/mamba/bin/dvc repro -s -f fineTuneModel_ablations@4 # visualizeImportance #runViennaRNA #
 ~/.local/share/mamba/bin/git add -u
-~/.local/share/mamba/bin/git commit -m "default - all extra features"
+~/.local/share/mamba/bin/git commit -m "(ablations) seq_only"
