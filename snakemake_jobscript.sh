5d47df87a0266cbe24ae3946fd0fb9bde1ee01a6
Logan Brase <logan.brase@manchester.ac.uk>
2025-10-20 08:00:09 +0100

    finished run using LLM_no_codon on decay dataset

--- snakemake_jobscript.sh
+++ snakemake_jobscript.sh
@@ -10,12 +10,13 @@
 #####################################################
 source ~/.bashrc
 conda activate rayTune
-~/.local/share/mamba/bin/dvc repro motifEnrichment
-~/.local/share/mamba/bin/dvc repro predict
-~/.local/share/mamba/bin/dvc repro motifEnrichment_highLowDecay
-~/.local/share/mamba/bin/dvc repro codonOnlyClassificationMLP
-~/.local/share/mamba/bin/dvc repro allExtraFeaturesClassificationMLP
-~/.local/share/mamba/bin/dvc repro extraFeaturesAndMotifCountsClassificationMLP
+ python scripts/manualTokenShuffle.py --params params.yaml --model_name_or_path output/ftModel_no_codons/best_spearmanr/ --data_dir output/data/decay --output_dir output/predict --scaler output/ftModel_no_codons/best_spearmanr/scaler.joblib --label tokenShuffle --extraFeatures output/data/ablations/no_codons.csv --mfe output/data/codons/vienna_features.csv
+#~/.local/share/mamba/bin/dvc repro motifEnrichment
+# ~/.local/share/mamba/bin/dvc repro predict
+# ~/.local/share/mamba/bin/dvc repro motifEnrichment_highLowDecay
+# ~/.local/share/mamba/bin/dvc repro codonOnlyClassificationMLP
+# ~/.local/share/mamba/bin/dvc repro allExtraFeaturesClassificationMLP
+# ~/.local/share/mamba/bin/dvc repro extraFeaturesAndMotifCountsClassificationMLP
 
 
 # ~/.local/share/mamba/bin/git add -u
